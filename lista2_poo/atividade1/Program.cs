14ae761f855304b1965f7a6c8949278da895dee3
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-01 10:01:10 -0300

    :sparkles: feat: adding program from first activity

---
+++ lista2_poo/atividade1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        EditorArquivo arquivo = new EditorArquivo("arquivo_1.txt");
+
+        arquivo.Open();
+
+        arquivo.Edit("hello world");
+
+        bool renomeado = arquivo.Rename("ArquivoInicial.txt");
+        Console.WriteLine("Arquivo renomeado? " + renomeado);
+
+        arquivo.Clear();
+
+        arquivo.Open();
+    }
+}
